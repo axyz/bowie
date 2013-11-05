358f5d24cb02b4f4c1d2c9aa805a0ec0e468deba
Andrea Moretti <axyzxp@gmail.com>
2013-11-05 22:13:37 +0100

    first working bowie install command

--- lib/bowie.rb
+++ lib/bowie.rb
@@ -1,19 +1,32 @@
 require "bowie/version"
 require "yaml"
 require "open-uri"
+require "fileutils"
+require "git"
 
 module Bowie
   
   @songs = YAML.parse(open("https://raw.github.com/axyz/bowie-songs/master/songs.yml")).to_ruby
-  
-  def self.test
-    puts "ciao"
+
+  def self.get_songs
+    @songs
   end
 
   def self.list
     @songs.each_key do |song|
       puts @songs[song]['name'] + ': ' + @songs[song]['description']
     end
+  end
+
+  def self.install(song)
+    name = @songs[song]['name']
+    url = @songs[song]['url']
+    path = "bowie_songs/#{name}"
+
+    puts "name: #{name}, url: #{url}" #TO-DELETE
+
+    FileUtils.mkdir_p(path)
+    Git.clone(url, path)
   end
 
 end
