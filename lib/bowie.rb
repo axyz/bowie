d7035212732b5a541719a38b5e8ff99c85904876
Andrea Moretti <axyzxp@gmail.com>
2013-11-09 15:35:18 +0100

    working local sonsg.yml

--- lib/bowie.rb
+++ lib/bowie.rb
@@ -7,35 +7,79 @@ require "logger"
 
 module Bowie
   
-  # Parse the remote list of packages
-  @songs = YAML.parse(open("https://raw.github.com/axyz/bowie-songs/master/songs.yml")).to_ruby
+  @songs
+  @local_songs
+  @local_lyrics
 
-  # Returns the list of packages
+  # Retrive the online registry of available songs
   def self.get_songs
-    @songs
+    begin
+      YAML.parse(open("https://raw.github.com/axyz/bowie-songs/master/songs.yml")).to_ruby
+    rescue
+      puts "ERROR: Cannot connect to github.com"
+    end
+  end
+
+  # Retrive the local registry of installed songs
+  def self.get_local_songs
+    begin
+      YAML.load_file('songs.yml')? YAML.load_file('songs.yml') : Array.new
+    rescue
+      begin
+        FileUtils.mkdir 'bowie_songs'
+      rescue 
+      end
+      FileUtils.touch 'songs.yml'
+      return Array.new
+    end
+  end
+
+  # Retrive the local lyrics file
+  def self.get_local_lyrics
+    begin
+      YAML.load_file('bowie_songs/lyrics.yml')? YAML.load_file('songs.yml') : Hash.new
+    rescue
+      begin
+        FileUtils.mkdir 'bowie_songs'
+      rescue
+      end
+      FileUtils.touch 'bowie_songs/lyrics.yml'
+      return Hash.new
+    end
   end
 
   # Return a printable list of available packages matching the [query]
   def self.search(query)
+    @songs = self.get_songs
     @songs.select { |el| /.*#{query}.*/ =~ el }
   end
 
   # Install the selected [song] in the bowie_songs directory
   def self.install(song)
+    @songs = self.get_songs
+    @local_songs = self.get_local_songs
     name = @songs[song]['name']
     url = @songs[song]['url']
     path = "bowie_songs/#{name}"
 
     FileUtils.mkdir_p(path)
     Git.clone(url, path)
+
+    @local_songs.push song
+    File.open("songs.yml", "w"){|f| YAML.dump(@local_songs, f)}
   end
 
   # Remove the selected package
   def self.uninstall(song)
+    @songs = self.get_songs
+    @local_songs = self.get_local_songs
     name = @songs[song]['name']
     path = "bowie_songs/#{name}"
 
     FileUtils.rm_rf(path) # use remove_entry_secure for security reasons?
+
+    @local_songs.delete song
+    File.open("songs.yml", "w"){|f| YAML.dump(@local_songs, f)}
   end
 
   # Update the selected package
