7b1f21f2b9ab4a8ed587619b82ba9c28d49f6821
Andrea Moretti <axyzxp@gmail.com>
2013-11-10 12:52:45 +0100

    utils.rb

---
+++ lib/bowie/utils.rb
@@ -0,0 +1,14 @@
+module Bowie
+
+    # Return an array with all the direcotry inside bowye_songs
+    def get_songs_dirs
+      result = []
+      Dir.foreach('bowie_songs') do |el|
+        unless (el == "." or el == ".." or el == "lyrics.yml")
+          result.push el
+        end
+      end
+      result
+    end
+
+end
